e6c1e388848d58d9c72f531881670aebe2ad28be
Sanjay-tone <sivasanjay22001@gmail.com>
2025-01-29 22:41:37 +0530

    updated reset button

--- src/component.js
+++ src/component.js
@@ -14,13 +14,18 @@ function Component() {
     setitems(items - 1)
   }
 
+  const handleclick = () => {
+    setitems(0)
+  }
+
   return (
     <>
       <div className="sec">
+        <h1>COUNTER APP</h1>
         <h1 id="items">{items}</h1>
         <button id="btn" onClick={handleincrement}>INCREMENT</button>
         <button id="btn1" onClick={handledecrement}>DECREMENT</button>
-
+        <button id="btn2" onClick={handleclick}>RESET</button>
       </div>
     </>
 
